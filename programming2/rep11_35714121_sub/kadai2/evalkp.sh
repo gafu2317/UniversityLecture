37802903ab78618588dc51a8a61c8c24b84e6471
gafu2317 <0002317rynkmk@gmail.com>
2024-10-13 03:35:45 +0900

    P2 １１回課題再提出

--- programming2/rep11_35714121_sub/kadai2/evalkp.sh
+++ programming2/rep11_35714121_sub/kadai2/evalkp.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 for i in `seq -w 1 30`; do # seqは先頭を0で埋めた01-30の文字列のリストを出力
-	echo n=$i
-	cat < input/${i}.txt # 入力ファイルを読み込んでコンソールに出力
-	java KP  < input/${i}.txt > output/${i}out.txt # ファイルに出力
+	java KP < input/${i}.txt > output/kpout${i}.txt # ファイルに出力
 done
