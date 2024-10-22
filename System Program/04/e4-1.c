a2ead621ac1db7fae8b8ae180c8a1b31f464f27f
gafu2317 <0002317rynkmk@gmail.com>
2024-10-22 11:46:59 +0900

    sp 4

--- System Program/04/e4-1.c
+++ System Program/04/e4-1.c
@@ -1,11 +1,19 @@
-int v1,v2,v3;
+#include <stdio.h> // ここを確認
+
+int v1, v2, v3;
 char v4;
 int *v5;
-void assign1(void){
-  v1=10;
-  v2=0x12345678;
-  v3=0b1011;
-  v4='A';
-  v5=&v1;
-  return;
+
+void assign1(void) {
+  v1 = 10;
+  v2 = 0x12345678;
+  v3 = 0b1011;
+  v4 = 'A';
+  v5 = &v1;
+}
+
+int main(void) {
+  assign1();
+  printf("v1: %d, v2: %d, v3: %d, v4: %c\n", v1, v2, v3, v4);
+  return 0;
 }
--- System Program/04/e4-1a-x64.s
+++ System Program/04/e4-1a-x64.s
@@ -3,18 +3,21 @@
 .text
 .globl assign1
 .type assign1, @function
+
 assign1:
-  mov DWORD PTR v1[rip], 10
-  mov DWORD PTR v2[rip], 0x12345678
-  mov DWORD PTR v3[rip], 0b1011
-  mov BYTE PTR v4[rip], 'A'
-  lea rax, v1[rip]
-  mov QWORD PTR v5[rip], rax
+  mov dword ptr [v1], 10
+  mov dword ptr [v2], 0x12345678
+  mov dword ptr [v3], 0b1011
+  mov byte ptr [v4], 'A'
+  lea rax, [v1]
+  mov qword ptr [v5], rax
   ret
-  .size assign1, .-assign1
-  .bss
-  .comm v1,4,4
-  .comm v2,4,4
-  .comm v3,4,4
-  .comm v4,1,1
-  .comm v5,8,8
+
+.size assign1, .-assign1
+
+.bss
+  .comm v1, 4
+  .comm v2, 4
+  .comm v3, 4
+  .comm v4, 1
+  .comm v5, 8
